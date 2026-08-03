7d940069792331712e882865fce80fe56e0b0835
Cedwards2527 <cedwards2527@gmail.com>
2026-08-03 17:33:47 -0400

    Create about button

--- crystal_light_and_lore/src/components/Header/Header.jsx
+++ crystal_light_and_lore/src/components/Header/Header.jsx
@@ -1,28 +1,34 @@
-import { Sparkles } from "lucide-react";
-import { CalendarHeart } from "lucide-react";
+import { Sparkles, CalendarHeart, User } from "lucide-react";
 import headerImage from "../../assets/header-image.jpg";
 import "./Header.css";
 
 function Header() {
   return (
-    <header
-      className="header"
-      style={{ "--header-image": `url(${headerImage})` }}
-    >
-      <img className="header__image" src={headerImage} alt="Header Image" />
-      <div className="header__content">
-        <div className="header__subtitle-row">
-          <Sparkles className="header__sparkles" />
-          <p className="header__subtitle">Psychic Medium & Oracle Reader</p>
-          <Sparkles className="header__sparkles" />
+    <section className="header__page">
+      <header
+        className="header"
+        style={{ "--header-image": `url(${headerImage})` }}
+      >
+        <img className="header__image" src={headerImage} alt="Header Image" />
+        <div className="header__content">
+          <div className="header__subtitle-row">
+            <Sparkles className="header__sparkles" />
+            <p className="header__subtitle">Psychic Medium & Oracle Reader</p>
+            <Sparkles className="header__sparkles" />
+          </div>
+          <h1 className="header__title">Crystal's Light & Lore</h1>
+          <button className="appointment__button">
+            <CalendarHeart className="calendar" strokeWidth={1.5} /> Book an
+            Appointment
+          </button>
         </div>
-        <h1 className="header__title">Crystal's Light & Lore</h1>
-        <button className="appointment__button">
-          <CalendarHeart className="calendar" strokeWidth={1.5} /> Book an
-          Appointment
+      </header>
+      <div className="about__content">
+        <button className="about__button">
+          <User className="user__icon" strokeWidth={1.5} /> Meet Crystal
         </button>
       </div>
-    </header>
+    </section>
   );
 }
 
