32e514444092c78ac057b8968f088f519161625e
Cedwards2527 <cedwards2527@gmail.com>
2026-08-11 14:31:30 -0400

    Create home page title

--- crystal_light_and_lore/src/pages/Home/Home.jsx
+++ crystal_light_and_lore/src/pages/Home/Home.jsx
@@ -4,11 +4,12 @@ import "./Home.css";
 function Home() {
   return (
     <section className="home__page">
-      <div className="home">
+      <div className="home__subtitle-row">
         <Sparkles className="home__sparkles" />
         <p className="home__subtitle">Light · Love · Guidance</p>
         <Sparkles className="home__sparkles" />
       </div>
+      <h2 className="home__title">Spiritual Readings</h2>
     </section>
   );
 }
