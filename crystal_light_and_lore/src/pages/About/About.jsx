2670bf2a46697b146c3ea6944ca699d9faf6760d
Cedwards2527 <cedwards2527@gmail.com>
2026-08-27 11:04:40 -0400

    Add about image

--- crystal_light_and_lore/src/pages/About/About.jsx
+++ crystal_light_and_lore/src/pages/About/About.jsx
@@ -1,11 +1,14 @@
 import BackToHome from "../../components/BackToHome/BackToHome.";
-
+import aboutImage from "../../assets/about-image.jpg";
 import "./About.css";
 
 function About() {
   return (
     <section className="about__page">
       <BackToHome />
+      <div className="about__container">
+        <img className="about__image" src={aboutImage} />
+      </div>
     </section>
   );
 }
