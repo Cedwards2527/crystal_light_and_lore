e32bdbc979a958fb52a30259885d8b4bc8404e3b
Cedwards2527 <cedwards2527@gmail.com>
2026-08-27 20:58:33 -0400

    Add sparkle to about page

--- crystal_light_and_lore/src/pages/About/About.jsx
+++ crystal_light_and_lore/src/pages/About/About.jsx
@@ -1,3 +1,4 @@
+import { Sparkles } from "lucide-react";
 import BackToHome from "../../components/BackToHome/BackToHome.";
 import aboutImage from "../../assets/about-image.jpg";
 import "./About.css";
@@ -10,6 +11,7 @@ function About() {
         <img className="about__image" src={aboutImage} />
         <h2 className="about__name">Crystal</h2>
         <p className="about__subtitle">Psychic Medium & Oracle Reader</p>
+        <Sparkles className="about__sparkles" strokeWidth={1.5} />
       </div>
     </section>
   );
