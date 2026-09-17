e6fcb84e988c75f4d6be6166dca5eaa3b1c1c7b3
Cedwards2527 <cedwards2527@gmail.com>
2026-09-17 17:39:17 -0400

    Add sun and title to card of the day page

--- crystal_light_and_lore/src/pages/CardOfTheDay/CardOfTheDay.jsx
+++ crystal_light_and_lore/src/pages/CardOfTheDay/CardOfTheDay.jsx
@@ -1,9 +1,15 @@
+import { Sun } from "lucide-react";
 import BackToHome from "../../components/BackToHome/BackToHome.";
+import "./CardOfTheDay.css";
 
 function CardOfTheDay() {
   return (
     <section className="daily__page">
       <BackToHome />
+      <div className="daily__container">
+        <Sun className="daily__sun" strokeWidth={1.2} />
+        <h2 className="daily__title">Daily Tarot Card</h2>
+      </div>
     </section>
   );
 }
